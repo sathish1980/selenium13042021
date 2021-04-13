4caecec82c857e72008cd9fb560bb9e2a4d19697
sathish1980 <kumar.sathish189@gmail.com>
2021-04-13 20:41:17 +0530

    added new file

---
+++ src/test/java/sample/ProjectMAven/gitclass.java
@@ -0,0 +1,14 @@
+package sample.ProjectMAven;
+
+public class gitclass {
+
+	public void add()
+	{
+		System.out.println("this is for git hub");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
